ca4a0aac170ef8556872e50aa1b88e898213c941
tamiroh <tamiroh71@gmail.com>
2026-08-16 21:54:21 +0900

    Enable more CSS rules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,8 +2,18 @@
 export default {
 	extends: ["stylelint-config-standard"],
 	plugins: ["./stylelint-plugin-local.mjs"],
+	reportNeedlessDisables: true,
+	reportInvalidScopeDisables: true,
+	reportDescriptionlessDisables: true,
 	rules: {
 		"local/selector-class-module-pattern": true,
+		"declaration-no-important": true,
+		"function-linear-gradient-no-nonstandard-direction": true,
+		"max-nesting-depth": 3,
+		"no-unknown-animations": true,
+		"no-unknown-custom-media": true,
+		"selector-max-id": [0],
+		"time-min-milliseconds": 100,
 	},
 	overrides: [
 		{
